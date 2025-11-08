59e2b490cf1647738284351d53dd7b97fe20492c
Tiago Santos <tiagosantos.eli@gmail.com>
2025-11-08 12:44:24 -0300

    Adiciona Botão de tema claro e escuro, assim como list card dos alunos nos cards da tela de turmas

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,7 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "turmas"
 import "confirm_modal"
+import "custom/theme_toggle"
+import "alunos_modal"
 
 console.log("✅ application.js carregado");
