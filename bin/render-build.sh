c4ce50adf3bc4a69c3f8722b5e959923be085e3f
jjeancarlos <jeanpastebin@gmail.com>
2025-11-25 04:00:20 -0300

    (ci): resolvendo erros de logs no render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-bundle install --without development test
+bundle config set without 'development test'
 bundle exec rails assets:precompile
+bundle exec rails db:prepare
+bundle exec rails db:seed
