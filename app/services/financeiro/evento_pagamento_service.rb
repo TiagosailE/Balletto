6f8e73bed0e2a74b1583386b3b35742f9d52b301
Tiago Santos <tiagosantos.eli@gmail.com>
2025-11-26 13:40:04 -0300

    Conserta bug de pagamento em eventos

--- app/services/financeiro/evento_pagamento_service.rb
+++ app/services/financeiro/evento_pagamento_service.rb
@@ -12,6 +12,7 @@ module Financeiro
       
       if pagamento
         pagamento.pag_valor_pago = (pagamento.pag_valor_pago || 0) + valor_pago
+        atualizar_status(pagamento)
         pagamento.save
       else
         pagamento = criar_novo_pagamento(valor_pago)
@@ -23,17 +24,29 @@ module Financeiro
     private
 
     def criar_novo_pagamento(valor_pago)
-      Pagamento.create(
+      pagamento = Pagamento.new(
         caixa_id: @params[:caixa_id],
         aluno_id: @aluno.alu_codigo,
         evento_id: @evento.EVE_CODIGO,
         pag_data: Time.current,
-        pag_valor: @evento.EVE_VALOR,
+        pag_valor: @evento.EVE_VALOR || 0,
         pag_valor_pago: valor_pago,
         pag_descricao: "Pagamento - #{@evento.EVE_NOME}",
         pag_tipo: 'Entrada',
         pag_metodo: @params[:metodo] || 'Dinheiro'
       )
+      
+      atualizar_status(pagamento)
+      pagamento.save
+      pagamento
+    end
+
+    def atualizar_status(pagamento)
+      if pagamento.pag_valor_pago >= pagamento.pag_valor
+        pagamento.pag_status = 'Pago'
+      else
+        pagamento.pag_status = 'Pendente'
+      end
     end
   end
 end
