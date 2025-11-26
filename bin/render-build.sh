45f3484075d48f853808944df771772845375426
jjeancarlos <jeanpastebin@gmail.com>
2025-11-26 13:19:40 -0300

    fix sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
+
 bundle binstubs bundler --force
 bundle config set without 'development test'
+bundle install
 bundle exec rails assets:precompile
 bundle exec rails db:prepare
 bundle exec rails db:seed
